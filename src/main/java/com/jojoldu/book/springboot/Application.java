e56856f2a817608d64ec3a3b7fc65b26781e57f6
haeyun0911 <haeyun0911@gmail.com>
2025-09-04 17:21:05 +0900

    변경

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -2,8 +2,10 @@ package com.jojoldu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 // 스프링 부트의 자동 설정, 스프링 brean 읽기와 생성을 모두 자동
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
