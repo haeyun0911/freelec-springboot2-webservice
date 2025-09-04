4e04cf5bd87e17d93b4798835ec10007d1532d94
haeyun0911 <haeyun0911@gmail.com>
2025-09-04 14:47:05 +0900

    gitignore등록

---
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -0,0 +1,14 @@
+package com.jojoldu.book.springboot.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//컨트롤러를 JSON으로 반환하는 컨트롤러로 만들어줌
+@RestController
+public class HelloController {
+    //HTTP Method인 Get 요청을 받을수 있는 api를 만들어줌
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
